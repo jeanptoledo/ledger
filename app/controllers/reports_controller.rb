4e0cb115e2b65e294e09980663e2698ea37692b5
Jean Toledo <jeanptoledo8@gmail.com>
2024-05-25 11:05:41 -0300

    Aula 20240525

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,6 @@
 class ReportsController < ApplicationController
   def balance
+    PersonMailer.before_report(current_user).deliver_now
     redirect_to root_path, notice: 'Não implementado'
   end
 end
